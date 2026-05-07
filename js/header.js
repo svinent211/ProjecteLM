013a04020719fb7f082b4346fed26700485bc8b1
SergiV <svinent21176@iesjoanramis.org>
2026-05-07 17:18:40 +0200

    Fer enllaços amb js, avançar reserves.html i començar contacte.html

--- js/header.js
+++ js/header.js
@@ -12,3 +12,25 @@ btnClose.addEventListener("click", () => {
     navMenu.classList.remove("display-block")
     btnClose.classList.add("display-none")
 });
+
+const aHome = document.querySelector("#aHome");
+const aReserves = document.querySelector("#aReserves");
+const aSNosaltres = document.querySelector("#aSobreNosaltres");
+const aCarta = document.querySelector("#aCarta");
+const aContacte = document.querySelector("#aContacte");
+
+aHome.addEventListener("click", ()=>{
+    window.location.href= "/Projecte/index.html";
+});
+aReserves.addEventListener("click", ()=>{
+    window.location.href= "/Projecte/reserves.html";
+});
+aSNosaltres.addEventListener("click", ()=> {
+    window.location.href= "/Projecte/sobreNosaltres.html";
+});
+aCarta.addEventListener("click", ()=> {
+    window.location.href = "/Projecte/carta.html";
+});
+aContacte.addEventListener("click", ()=> {
+    window.location.href= "/Projecte/contacte.html";
+});
